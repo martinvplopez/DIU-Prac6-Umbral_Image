55a50fdf09eeb5d0272df6540625d09bd9d6620f
martinvplopez <vanpu3d@gmail.com>
2021-10-30 10:59:58 +0200

    Commit

--- src/main/java/com/mycompany/practica6/Practica6.java
+++ src/main/java/com/mycompany/practica6/Practica6.java
@@ -1,15 +1,9 @@
 package com.mycompany.practica6;
 
-import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import org.opencv.core.Core;
 
 /**
  *
@@ -17,22 +11,26 @@ import org.opencv.core.Core;
  */
 public class Practica6 extends javax.swing.JFrame {
 
-    JFileChooser fc= new JFileChooser();
-    FileNameExtensionFilter filtro=null;
+    private JFileChooser fc= new JFileChooser();
+    private FileNameExtensionFilter filtro=null;
+    private File fichero;
+    
     public Practica6() {
         initComponents();
         setLocationRelativeTo(null);
         filtro=new FileNameExtensionFilter("Imágenes (jpg, png, jpeg)", "jpg", "png", "jpeg");
         fc.setFileFilter(filtro);
         avisoLabel.setVisible(false);
-        nu.pattern.OpenCV.loadShared(); 
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        guardarItem.setEnabled(false);
+        umbralItem.setEnabled(false);
+        verItem.setEnabled(false);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        umbral1 = new com.mycompany.practica6.Umbral();
         avisoLabel = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         archivoMenu = new javax.swing.JMenu();
@@ -49,6 +47,17 @@ public class Practica6 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Umbralización Imagen");
+
+        javax.swing.GroupLayout umbral1Layout = new javax.swing.GroupLayout(umbral1);
+        umbral1.setLayout(umbral1Layout);
+        umbral1Layout.setHorizontalGroup(
+            umbral1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 772, Short.MAX_VALUE)
+        );
+        umbral1Layout.setVerticalGroup(
+            umbral1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 397, Short.MAX_VALUE)
+        );
 
         avisoLabel.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         avisoLabel.setText("Archivo seleccionado no es una imagen válida. Comprueba la extensión");
@@ -136,16 +145,23 @@ public class Practica6 extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(avisoLabel)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(32, 32, 32)
+                        .addComponent(umbral1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(87, 87, 87)
+                        .addComponent(avisoLabel)))
                 .addContainerGap(59, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(140, 140, 140)
+                .addGap(27, 27, 27)
+                .addComponent(umbral1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(avisoLabel)
-                .addContainerGap(199, Short.MAX_VALUE))
+                .addContainerGap(26, Short.MAX_VALUE))
         );
 
         pack();
@@ -157,8 +173,13 @@ public class Practica6 extends javax.swing.JFrame {
         int rDialog=fc.showOpenDialog(null);
         if (rDialog==JFileChooser.APPROVE_OPTION){
             System.out.println("Archivo seleccionado");
-            File fichero=  fc.getSelectedFile();
-            checkFileExtension(fichero);
+            fichero=  fc.getSelectedFile();
+            String ext=checkFileExtension(fichero);
+            if(ext==""){
+                avisoLabel.setText("¡Intentando abrir algo que no es una imagen!");
+                avisoLabel.setVisible(true);
+            }
+            
             
         }
         if (rDialog==JFileChooser.CANCEL_OPTION){
@@ -167,7 +188,10 @@ public class Practica6 extends javax.swing.JFrame {
         }
         
         if(!avisoLabel.isVisible()){
-            
+            umbral1.openImage(fichero);
+            guardarItem.setEnabled(true);
+            umbralItem.setEnabled(true);
+            verItem.setEnabled(true);
         }
         
     }//GEN-LAST:event_abrirItemActionPerformed
@@ -177,7 +201,7 @@ public class Practica6 extends javax.swing.JFrame {
         int rDialog=fc.showSaveDialog(null);
         if (rDialog==JFileChooser.APPROVE_OPTION){
             System.out.println("Archivo seleccionado");
-            File fichero=  fc.getSelectedFile();
+            fichero=  fc.getSelectedFile();
             String ext=checkFileExtension(fichero);
             if(ext==""){
                 avisoLabel.setText("Guardando algo que no es una imagen");
@@ -189,16 +213,15 @@ public class Practica6 extends javax.swing.JFrame {
                         ,"Sobrescribir Fichero", JOptionPane.YES_NO_OPTION);
                 if(res2==JOptionPane.YES_OPTION){
                     System.out.println("Imagen Guardada sobreescrita");
+                    umbral1.saveImage(fichero.getAbsolutePath());
+                    guardarItem.setEnabled(false);
                 }else{
+                    umbral1.saveImage(fichero.getAbsolutePath());
+                    guardarItem.setEnabled(false);
                     System.out.println("Imagen Guardada");
 
                 }
             }
-            /*try{
-                //ImageIO.write((BufferedImage)imagen,ext ,fichero);
-            }catch(IOException e){
-                System.out.println("Error al guardar imagen");
-            }*/
         }
         if (rDialog==JFileChooser.CANCEL_OPTION){
             avisoLabel.setText("Nada seleccionado");
@@ -211,7 +234,19 @@ public class Practica6 extends javax.swing.JFrame {
     }//GEN-LAST:event_closeItemActionPerformed
 
     private void umbralItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_umbralItemActionPerformed
-        // TODO add your handling code here:
+        String input = JOptionPane.showInputDialog(rootPane, "Introduzca el valor de umbral.");
+        if(input!=null){
+            try{
+                int umbral = Integer.parseInt(input);
+                umbral1.umbralizarImagen(umbral);
+                guardarItem.setEnabled(true);
+            }catch(NumberFormatException e){
+                guardarItem.setEnabled(false);
+                JOptionPane.showMessageDialog(rootPane, "ERROR: Introducir solo números"
+                        , "ERROR", JOptionPane.ERROR_MESSAGE);
+                umbralItemActionPerformed(evt);
+            }
+        }      
     }//GEN-LAST:event_umbralItemActionPerformed
 
     private void verItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_verItemActionPerformed
@@ -305,6 +340,7 @@ public class Practica6 extends javax.swing.JFrame {
     private javax.swing.JMenuItem guardarItem;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
+    private com.mycompany.practica6.Umbral umbral1;
     private javax.swing.JMenuItem umbralItem;
     private javax.swing.JMenuItem verItem;
     private javax.swing.JMenu verMenu;
--- src/main/java/com/mycompany/practica6/Umbral.java
+++ src/main/java/com/mycompany/practica6/Umbral.java
@@ -1,9 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.practica6;
 
-/**
- *
- * @author marti
- */
-public class Umbral {
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+
+public class Umbral extends JPanel{
+    
+    private Mat imageMat, imageMatOriginal;
+    private BufferedImage imagen;
+    private BufferedImage imagen2;
+    
+    public Umbral(){    
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(imagen, 0, 0, null);
+    }
+    
+    public void openImage(File fichero){
+        try {
+            imagen=ImageIO.read(fichero);
+            imagen2 = imagen;
+            repaint();
+        } catch (IOException ex) {
+            Logger.getLogger(Umbral.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void originalImageFilter(){
+        imageMat = imageMatOriginal.clone();
+        imagen = (BufferedImage) HighGui.toBufferedImage(imageMat);
+        this.repaint();
+    }
+    
+    void umbralizarImagen(int value) {
+        imagen2 = ImagenUmbralizada(imagen, value);
+        repaint(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void closeImage(){
+        imagen=null;
+        imageMat=null;
+        this.repaint();
+    }
+    
+    public void saveImage(String path){
+        imageMatOriginal = imageMat.clone();
+        Imgcodecs.imwrite(path,imageMat);
+    }
+    public static Mat bufferedImageAMat(BufferedImage imagenOriginal) {
+        Mat mat = new Mat(imagenOriginal.getHeight(), imagenOriginal.getWidth(), CvType.CV_8UC3);
+        byte[] data = ((DataBufferByte) imagenOriginal.getRaster().getDataBuffer()).getData();
+        mat.put(0, 0, data);
+        return mat;
+    }
+    
+    private static BufferedImage ImagenUmbralizada(BufferedImage imagenOriginal, int umbral) throws UnsupportedOperationException {
+        try {
+            Mat imagenMat = bufferedImageAMat(imagenOriginal);
+            Mat imagenMatGris = new Mat(imagenMat.rows(), imagenMat.cols(), CvType.CV_8U);
+            Mat imagenMatUmbral = new Mat(imagenMat.rows(), imagenMat.cols(), CvType.CV_8U);
+            Imgproc.cvtColor(imagenMat, imagenMatGris, Imgproc.COLOR_BGR2GRAY);
+            Imgproc.threshold(imagenMatGris, imagenMatUmbral, umbral, 255, Imgproc.THRESH_BINARY);
+            return (BufferedImage) HighGui.toBufferedImage(imagenMatUmbral);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getClass().getName() + " " + e.getMessage());
+            return null;
+        }
+    }
+        
+    public BufferedImage getImage() {
+        return imagen2;
+    }
+
     
 }
+
+
